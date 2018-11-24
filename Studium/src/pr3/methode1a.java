a1306d652c355a1bfe5ef4392d42aa5cb57654d0
dknr1 <dknr1@T-1000>
2018-11-24 06:48:56 +0100

    Wieder zum Absender

--- Studium/src/pr3/methode1a.java
+++ Studium/src/pr3/methode1a.java
@@ -4,6 +4,7 @@ public class methode1a {
 
 	public static void main(String[] args) {
 		System.out.println("Hi du auf dem groﬂen PC, ich bin ein Surface");
+		System.out.println("Hi, nett dich kennen zu lernen");
 	}
 
 }
