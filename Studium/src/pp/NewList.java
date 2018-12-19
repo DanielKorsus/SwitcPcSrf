57a507008201c21b2ebc7e96ad38eb36ec120ae5
dknr1 <dknr1@141.19.147.141>
2018-12-19 09:50:11 +0100

    PP Aufgabe ohne Lösung

---
+++ Studium/src/pp/NewList.java
@@ -0,0 +1,18 @@
+package pp;
+
+public interface NewList<T> {
+	public T get(int i);
+
+	public void add(T e);
+
+	public void mod(int i, T e);
+
+	public default T inspect(T e) {
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException ex) {
+			// ... nothing ...
+		}
+		return e;
+	}
+}
